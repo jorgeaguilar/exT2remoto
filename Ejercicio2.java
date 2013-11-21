3b87fdb6b27ea88ca1363980d0021562fef027bb
jorgeaguilar <jorge_aguilar_carvajal@hotmail.com>
2013-11-21 09:42:30 +0100

    modificación

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -5,6 +5,7 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejercicio2");
+		System.out.println("Modificación")
 	}
 
 }
